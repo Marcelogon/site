4ec0e6e5b1b1e8fad978e63eb65c523185084dda
Marcelogon <marceloportalcamp@hotmail.com>
2019-09-03 22:07:40 -0300

    Add files via upload

---
+++ portalcampweb/build/precache-manifest.4483e7bfb0ebef3a0a5935c9ec815227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "56fc96d9962a575c63f5",
+    "url": "/static/js/main.56fc96d9.chunk.js"
+  },
+  {
+    "revision": "1e830d9bf3b34abd0a08",
+    "url": "/static/js/2.1e830d9b.chunk.js"
+  },
+  {
+    "revision": "56fc96d9962a575c63f5",
+    "url": "/static/css/main.225fb158.chunk.css"
+  },
+  {
+    "revision": "1e830d9bf3b34abd0a08",
+    "url": "/static/css/2.ca94a7b6.chunk.css"
+  },
+  {
+    "revision": "ecac65ac6e77362cf678b7048df8a638",
+    "url": "/index.html"
+  }
+];
